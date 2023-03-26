6eacb9eb345dbfe7bc7caa4c16e9a3ce51eb0904
alexbushi <alexbush@udel.edu>
2023-03-26 17:31:47 -0400

    build a navbar

---
+++ video-game-app/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { HStack, Image, Text } from '@chakra-ui/react'
+import logo from '../assets/logo.webp'
+
+const NavBar = () => {
+  return (
+    <HStack>
+        <Text>NavBar</Text>
+        <Image src={logo} boxSize='60px' alt="logo"/>
+    </HStack>
+  )
+}
+
+export default NavBar
