3e84ad05434a620a350d1685144f0d43ea9a12ea
alexbushi <alexbush@udel.edu>
2023-03-30 16:52:21 -0400

    Change document title

--- video-game-app/src/services/api-client.ts
+++ video-game-app/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "a8281442e4964b4b869f4ae6e1cb23e8",
+    key: "c7b18323a47d40c394ea5b019646b1f5",
   },
 });
